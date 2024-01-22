e3badbd79bb2257507e3e988684ac1572ffe57ef
Pleng <pacharaprompang1@gmail.com>
2024-01-22 14:01:07 +0700

    Update Hello Git in C and Hello Git in C++

---
+++ code-02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout : console output
+    // endl : end line '\n'
+    // compile : g++ code-02.cpp -o code-02.out
+    // run : ./code-02.out
+    cout << "Hello Git" << endl;
+
+}
